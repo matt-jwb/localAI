c1df81585d52be2baa16c10127cae1bd89f5d6c5
matt-jwb <mjwb2bf@gmail.com>
2025-06-28 19:20:01 +0100

    Added support for more models

--- download.py
+++ download.py
@@ -1,5 +1,33 @@
+import os
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-model = "gpt2"
-AutoTokenizer.from_pretrained(model, cache_dir="./local_model")
-AutoModelForCausalLM.from_pretrained(model, cache_dir="./local_model")
+
+def get_path(loc):
+    current_directory = os.getcwd()
+    relative_path = os.path.join(current_directory, loc)
+    normalised_path = os.path.normpath(relative_path)
+    if os.path.commonpath([current_directory, normalised_path]) == current_directory:
+        return normalised_path
+    else:
+        raise Exception("Invalid Location")
+
+def save_model(m, loc):
+    try:
+        AutoTokenizer.from_pretrained(m, cache_dir=loc)
+        AutoModelForCausalLM.from_pretrained(m, cache_dir=loc)
+    except Exception as ex:
+        print(f"There was a problem downloading the model: {ex}")
+
+def download():
+    model = input("Input a model >>  ")
+    location = input("Input save location >>  ")
+    try:
+        location = get_path(location)
+    except Exception as e:
+        print(f"There was a problem with the location: {e}")
+    while model != "quit":
+        save_model(model, location)
+        model = input("Input a model >> ")
+        location = input("Input save location >>  ")
+
+download()
--- main.py
+++ main.py
@@ -3,35 +3,57 @@ import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
 
-def get_latest_snapshot(model_dir="./local_model/models--gpt2"):
-    snapshots_dir = os.path.join(model_dir, "snapshots")
-    snapshot_folders = [f for f in os.listdir(snapshots_dir) if os.path.isdir(os.path.join(snapshots_dir, f))]
+class LLM:
+    def __init__(self, model_location):
+        self.model_path = self.get_latest_snapshot(model_location)
+        self.tokenizer = AutoTokenizer.from_pretrained(self.model_path)
+        self.model = AutoModelForCausalLM.from_pretrained(self.model_path)
 
-    latest_snapshot = max(snapshot_folders, key=lambda x: x)
-    return os.path.join(snapshots_dir, latest_snapshot)
+    def get_latest_snapshot(self, model_dir):
+        for subdir in os.listdir(model_dir):
+            subdir_path = os.path.join(model_dir, subdir)
 
-model_path = get_latest_snapshot()
+            if os.path.isdir(subdir_path):
+                snapshots_dir = os.path.join(subdir_path, 'snapshots')
+                if os.path.isdir(snapshots_dir):
+                    snapshot_folders = [f for f in os.listdir(snapshots_dir) if os.path.isdir(os.path.join(snapshots_dir, f))]
 
-tokenizer = AutoTokenizer.from_pretrained(model_path)
-model = AutoModelForCausalLM.from_pretrained(model_path)
+                    latest_snapshot = max(snapshot_folders, key=lambda x: x)
+                    return os.path.join(snapshots_dir, latest_snapshot)
+        return None
+
+    def generate_response(self, prompt):
+        inputs = self.tokenizer.encode(prompt, return_tensors="pt")
+
+        attention_mask = torch.ones(inputs.shape, device=inputs.device)
+
+        pad_token_id = self.tokenizer.eos_token_id
+
+        with torch.no_grad():
+            outputs = self.model.generate(inputs, max_length=150, num_return_sequences=1, no_repeat_ngram_size=2,
+                                     attention_mask=attention_mask, pad_token_id=pad_token_id)
+
+        return self.tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+def get_path(loc):
+    current_directory = os.getcwd()
+    relative_path = os.path.join(current_directory, loc)
+    normalised_path = os.path.normpath(relative_path)
+    if os.path.commonpath([current_directory, normalised_path]) == current_directory:
+        return normalised_path
+    else:
+        raise Exception("Invalid Location")
 
 
-def generate_response(prompt):
-    inputs = tokenizer.encode(prompt, return_tensors="pt")
-
-    attention_mask = torch.ones(inputs.shape, device=inputs.device)
-
-    pad_token_id = tokenizer.eos_token_id
-
-    with torch.no_grad():
-        outputs = model.generate(inputs, max_length=150, num_return_sequences=1, no_repeat_ngram_size=2, attention_mask=attention_mask, pad_token_id=pad_token_id)
-
-    return tokenizer.decode(outputs[0], skip_special_tokens=True)
-
-
-text = input(">> ")
-while text != "quit":
-    response = generate_response(text)
-    print(response)
+def main():
+    location = input("Input model location >>  ")
+    location = get_path(location)
+    model_instance = LLM(location)
 
     text = input(">> ")
+    while text != "quit":
+        response = model_instance.generate_response(text)
+        print(response)
+        text = input(">> ")
+
+main()
