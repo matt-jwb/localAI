4c49243ef5ab506b9cd3c4e94a3d4ca9159f430d
matt-jwb <mjwb2bf@gmail.com>
2025-06-29 18:34:18 +0100

    Added support for Hugging Face Token

--- download.py
+++ download.py
@@ -1,5 +1,7 @@
 import os
 from transformers import AutoTokenizer, AutoModelForCausalLM
+from huggingface_hub import login, whoami
+from huggingface_hub.utils import LocalTokenNotFoundError, HfHubHTTPError
 
 
 def get_path(loc):
@@ -18,7 +20,20 @@ def save_model(m, loc):
     except Exception as ex:
         print(f"There was a problem downloading the model: {ex}")
 
+def is_logged_in():
+    try:
+        whoami()
+        return True
+    except (LocalTokenNotFoundError, HfHubHTTPError):
+        return False
+
+def try_login():
+    token = input("Input your Hugging Face token >>  ")
+    login(token=token, add_to_git_credential=True)
+
 def download():
+    if not is_logged_in():
+        try_login()
     model = input("Input a model >>  ")
     location = input("Input save location >>  ")
     try:
